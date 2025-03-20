17913d29bc1d87191bd3fd61d4bfce7c2462eb3f
[Dillon Reading] <dillreading@gmail.com>
2025-03-20 16:45:27 -0600

    figuring out how to do ish with key presses

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import { Keyboard } from './Keyboard'
 import { Phrase } from "./Phrase.jsx";
+import {useEffect, useState} from "react";
 
 export function App() {
+    const [keyPressed, setKeyPressed] = useState(null)
+    
+    useEffect(() => {
+        function keydown(e) {
+            if (e.repeat) return;
+            setKeyPressed(e.key)
+        }
+        function keyup(e) {
+            setKeyPressed(null)
+        }
+
+        window.addEventListener("keyup", keyup);
+        window.addEventListener("keydown", keydown);
+
+        return () => {
+            window.removeEventListener("keydown", keydown);
+            window.removeEventListener("keyup", keyup);
+        };
+    }, []);
     return (
         <div>
             <Phrase />
